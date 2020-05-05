e31e80848399c506f8d7fd11297cb6198e7795b2
Filip Fidkowski <tropok@gmail.com>
2020-05-05 14:45:41 +0200

    Add project files.

---
+++ BusinessDashboardAPI/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using BusinessDashboardAPI.Services;
+using BusinessDashboardAPI.Models;
+using Microsoft.Extensions.Logging;
+
+namespace BusinessDashboardAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(IUserService userService, ILogger<UsersController> logger)
+        {
+            _userService = userService;
+            _logger = logger;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]AuthenticateModel model)
+        {
+            var user = _userService.Authenticate(model.Username, model.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+    }
+}
---
+++ BusinessDashboardAPI/Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace BusinessDashboardAPI.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
